1acdfb52eb2b260d63550dcca4992042a16c938d
aishanoor1439 <aisha.noor1439@gmail.com>
2025-05-29 05:19:29 +0500

    First Commi
    added project files

---
+++ src/main/java/com/mycompany/mavenproject44/Mavenproject44.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject44;
+
+/**
+ *
+ * @author ABC
+ */
+public class Mavenproject44 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
